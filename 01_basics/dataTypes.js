344aa415c4c233331107003e2a35ef3385d6d0ab
Rahul Kumar <127291884+rahul95rajpoot@users.noreply.github.com>
2023-12-16 03:38:42 +0000

    dataypes and variables

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+let name="rahul"
+let age=23
+
+/*
+data types-
+
+number=>2 to power 53
+string=""
+boolean=true/false
+null=standalone value
+undefined=
+symbol=unique
+
+object
+
+*/
+console.log (typeof 'rahul');//string
+console.log (typeof null);//object
---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId=123
+let accountEmail="rahul@gmail.com"
+let accountPass="rahul"
+accountCity="punjab"
+
+// accountId=2
+accountEmail="rhauk@gmail.com"
+accountPass="12" 
+accountCity="mahoba"
+
+console.table([accountEmail,accountPass,accountCity]);
+console.log(accountId);
